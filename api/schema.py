5090e25d634ec7d70af9741ce501e88a07be4556
Jose Godoy <josegodoy@alumnos.uai.cl>
2021-04-29 13:57:42 -0400

    ariande setup

---
+++ api/schema.py
@@ -0,0 +1,15 @@
+from ariadne import QueryType, make_executable_schema
+
+type_defs = """
+    type Query {
+        hello: String!
+    }
+"""
+
+query = QueryType()
+
+@query.field("hello")
+def resolve_hello(*_):
+    return "Hello world!"
+
+schema = make_executable_schema(type_defs, query)
